6765869f6aed5e040709bea03f4b4faa3ff66ac0
Josefine Lantz <jo.lantz@icloud.com>
2022-02-20 13:06:27 +0100

    set up react app

---
+++ src/tokens/all20.js
@@ -0,0 +1,8 @@
+/* Component for mapping ERC20-tokens to contract ABI.
+*/
+import MoolahCoin from "../contracts/MoolahCoin";
+
+const Tokens20 = [
+	MoolahCoin
+];
+export default Tokens20;
---
+++ src/tokens/all721.js
@@ -0,0 +1,7 @@
+/* Component for mapping ERC721 tokens to contract ABI.
+*/
+import Condos from "../contracts/Condos";
+const Tokens721 = [
+	Condos
+];
+export default Tokens721; 
